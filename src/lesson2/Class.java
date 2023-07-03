eddbafcd201f24c977819b2be8d8014b5835e5c3
katarina <ktrnmillet@gmail.com>
2023-07-03 17:57:39 +0300

    Initial commit

---
+++ src/lesson2/Class.java
@@ -0,0 +1,15 @@
+package lesson2;
+
+public class Class {
+    System.out.println
+    int x = (2 - 5*(9%4 - 7))%3;
+    int y = x + 3 * 2 - 46 % 2;
+    int z = x%y*2;
+
+    //
+    int x = 5;
+    int y = 4;
+    x = x++ - y++ + ++x;
+    y+=3;
+    int z = --x + --y;
+}
