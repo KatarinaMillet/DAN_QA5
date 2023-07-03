26b6e83a1848c70b37c664761f61e6c57cede336
katarina <ktrnmillet@gmail.com>
2023-07-03 19:12:49 +0300

    1

--- src/lesson2/Class.java
+++ src/lesson2/Class.java
@@ -1,15 +1,7 @@
 package lesson2;
 
 public class Class {
-    System.out.println
-    int x = (2 - 5*(9%4 - 7))%3;
-    int y = x + 3 * 2 - 46 % 2;
-    int z = x%y*2;
+    public static void main(String[] args) {
 
-    //
-    int x = 5;
-    int y = 4;
-    x = x++ - y++ + ++x;
-    y+=3;
-    int z = --x + --y;
+    }
 }
